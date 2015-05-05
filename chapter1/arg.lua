fd429b0ed56a35eff97958d21b45af47f87f1e4e
jdgwartney <davidg@boundary.com>
2015-05-05 10:09:10 -0700

    check

---
+++ chapter1/arg.lua
@@ -0,0 +1,8 @@
+print('args[-3]' .. tostring(arg[-3]))
+print('args[-2]' .. tostring(arg[-2]))
+print('arg[-1]'  .. tostring(arg[-1]))
+print('arg[0]'   .. tostring(arg[0]))
+print('arg[1]'   .. tostring(arg[1]))
+print('arg[2]'   .. tostring(arg[2]))
+print('arg[3]'   .. tostring(arg[3]))
+
---
+++ chapter1/lib1.lua
@@ -0,0 +1,9 @@
+-- Normalize the input values
+function norm(x, y)
+  return (x^2 + y^2)^0.5
+end
+
+-- Multiple the input argument by 2
+function twice(x)
+  return 2*x
+end
