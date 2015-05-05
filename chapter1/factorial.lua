e5a1a30a78b0847c596a2446d776f210150408ab
jdgwartney <davidg@boundary.com>
2015-05-05 10:18:38 -0700

    Add example function for factorial

---
+++ chapter1/factorial.lua
@@ -0,0 +1,14 @@
+-- defines a factorial function
+
+function fact(n)
+  if n == 0 then
+    return 1
+  else
+    return n * fact(n - 1)
+  end
+end
+
+print("enter a number:") -- reads a number
+a = io.read("*n")
+print(fact(a))
+
