77aba9d70b7eb265d9fa8dd0ba30d57e29b84f2b
jdgwartney <davidg@boundary.com>
2015-05-06 16:42:23 -0700

    Correct escaping of keys ; add output of table

--- chapter3/ex-3.8.lua
+++ chapter3/ex-3.8.lua
@@ -1,16 +1,20 @@
 
 escape_seq = {
-["\a"] = "Bell",
-["\b"] = "Backspace",
-["\f"] = "Form Feed",
-["\n"] = "New Line",
-["\t"] = "Horizontal Tab",
-["\r"] = "Carriage Return",
-["\\"] = "Back Slash",
-["\""] = "Double Quote",
-["\'"] = "Single Quite",
-["\v"] = "Vertical Tab",
+["\\a"] = "Bell",
+["\\b"] = "Backspace",
+["\\f"] = "Form Feed",
+["\\n"] = "New Line",
+["\\t"] = "Horizontal Tab",
+["\\r"] = "Carriage Return",
+["\\\\"] = "Back Slash",
+["\\\""] = "Double Quote",
+["\\\'"] = "Single Quote",
+["\\v"] = "Vertical Tab",
 }
+
+for k,v in pairs(escape_seq) do
+  print(k,v)
+end
 
 
 
